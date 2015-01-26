e584c59afeefd54aa258f0327dd85589ed6d3c9f
Charles R. Portwood II <charlesportwoodii@ethreal.net>
2015-01-26 19:16:21 +0000

    Updating parser script path

--- parser.sh
+++ parser.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 # This is a bash wrapper for parser.php
 #
-$(which git) pull && $(which php) parser.php
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+$(which cd) $DIR && $(which git) pull
+$(which cd) $DIR && $(which php) parser.php
